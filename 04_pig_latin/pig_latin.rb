685d68cb2f03d156f95cc3c5f0ee84e8f41a36cd
Roka <RokaRostuk@gmail.com>
2018-07-11 19:54:24 +0300

    Refactoring Pig Latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,11 @@
 def translate(sentence)
   sentence.gsub(/\w+/) do |word|
     capitalized = word.match(/^[A-Z]/)
-    word[0].downcase!
     word.sub!(
       /
       ^([^aeio]*) # select all consonants at the beginning
       (.*)$       # select rest characters
-      /x, '\2\1ay'
+      /xi, '\2\1ay'
     )
     word.capitalize! if capitalized
     word
